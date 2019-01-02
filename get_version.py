393b9b92b4282a30eeddc2fcab3d929ae1a97395
Philipp A <flying-sheep@web.de>
2019-01-02 13:44:47 +0100

    test black

--- get_version.py
+++ get_version.py
@@ -51,7 +51,7 @@ def get_version_from_dirname(name, parent):
     parent = parent.resolve()
     logger.info(f"dirname: Trying to get version of {name} from dirname {parent}")
 
-    name_re = name.replace('_', '[_-]')
+    name_re = name.replace("_", "[_-]")
     re_dirname = re.compile(f"{name_re}-{RE_VERSION}$")
     if not re_dirname.match(parent.name):
         logger.info(f"dirname: Failed; Does not match {re_dirname!r}")
